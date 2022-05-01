6c2d7cb78cb3dee467138f5dd91ee195d31a1d19
Paulo Scariot <paulohs.trabalhos@gmail.com>
2022-05-01 18:06:36 -0300

    finished application

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -12,7 +12,7 @@ export class ProductCreateComponent implements OnInit {
 
   product: Product = {
     name: '',
-    price: null as any
+    price: null!
   }
 
   constructor(private productService: ProductService,
